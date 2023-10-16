d69e24adaeaaa43aba1606cd38b456b6dea35ac4
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-10-16 07:54:46 +0000

    admin_user_review

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,18 +1,18 @@
 class Admin::ReviewsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-    @review = Review.all
+    @reviews = Review.all
   end
-  
+
   def show
     @review = Review.find(params[:id])
   end
-  
+
   def edit
     @review = Review.find(params[:id])
   end
-  
+
   def update
     if @review.update(review_params)
       redirect_to admin_review_path(@review)
@@ -20,9 +20,9 @@ class Admin::ReviewsController < ApplicationController
       render :edit
     end
   end
-  
+
   private
-  
+
   def review_params
     params.require(:reivew).permit(:title, :explanation, :store_name, :address, :score, :price,)
   end
