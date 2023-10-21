18dc4be6d1e77108b67bdc48b3adae51688905f6
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-10-21 10:15:23 +0000

    layouts_reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -26,13 +26,14 @@ class Review < ApplicationRecord
   end
 
 
-  def get_image(width, height)
+
+  def get_review_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [width, height]).processed
-  end
+     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+   image
+ end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
