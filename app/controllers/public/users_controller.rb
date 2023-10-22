126477d95bbc5f12a87f749cea5b4adad504ca7e
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-10-22 09:52:24 +0000

    favorites-index

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -33,32 +33,34 @@ class Public::UsersController < ApplicationController
   end
 
   def favorites
+    @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:review_id)
     @favorite_reviews = Review.find(favorites)
+    @review = Review.find(params[:id])
   end
 
 
   private
 
-  def ensure_guest_user
-    @user = User.find(params[:id])
-    if @user.name == "guestuser"
-      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    def ensure_guest_user
+      @user = User.find(params[:id])
+      if @user.name == "guestuser"
+        redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+      end
     end
-  end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :profile_image)
-  end
-
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to reviews_path
+    def user_params
+      params.require(:user).permit(:name, :email, :profile_image)
     end
-  end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def is_matching_login_user
+      user = User.find(params[:id])
+      unless user.id == current_user.id
+        redirect_to reviews_path
+      end
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
