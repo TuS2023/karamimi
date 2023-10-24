6e71265fde9e47fb48b12a1f43bba359bfba14b9
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-10-24 11:39:38 +0000

    background-image_score_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable#, :validatable ゲストログイン時バリデーションエラー発生のため
-
+         :recoverable, :rememberable, :validatable 
+         
   has_one_attached :profile_image
 
   def self.guest
