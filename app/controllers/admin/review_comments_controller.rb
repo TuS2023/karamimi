9a0cbcdf5bfb61b7a1f5221f271be92422aa3b47
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-10-26 03:54:36 +0000

    admin_edit_layouts

--- app/controllers/admin/review_comments_controller.rb
+++ app/controllers/admin/review_comments_controller.rb
@@ -1,12 +1,13 @@
 class Admin::ReviewCommentsController < ApplicationController
-  
+
   def destroy
     comment = ReviewComment.find(params[:id])
     comment.destroy
+    redirect_to request.referer
   end
-  
+
   private
-  
+
   def review_comment_params
     params.require(:review_comment).permit(:comment)
   end
